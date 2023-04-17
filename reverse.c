88a472b3d261c919a608b85ef96c0cc2a84a0115
marufhasan566 <marufhasan.mh1@gmail.com>
2023-04-17 21:20:33 +0600

    reverse an array

---
+++ reverse.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int i = 0, j = n - 1, temp = i;
+    while (i < j)
+    {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
